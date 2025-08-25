7943f36015b5921f693a990ac6459fab61abdd27
MohammedSaif29 <2400031209@kluniversity.in>
2025-08-25 15:28:43 +0530

    initial push

---
+++ loginpgescript.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Simple Login Page</title>
+</head>
+<body>
+    <h1>LOGIN PAGE</h1>
+    <br/>
+    <br/>
+    <form action="#" method="post">
+        <fieldset>
+            <legend>Login Details</legend>
+
+            <label for="username">User Name:</label>
+            <input type="text" id="username" name="username" required>
+            <br/><br/>
+
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            <br/><br/>
+
+            <input type="submit" value="Submit">
+            <input type="reset" value="Reset">
+        </fieldset>
+    </form>
+</body>
+</html>
